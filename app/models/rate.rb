dc46a1ec7eee166a710523d0757f5d13417ddeaf
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-10-26 18:55:24 -0400

    detalles varios

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,6 +1,4 @@
 class Rate < ApplicationRecord
   has_many :bookings, dependent: :destroy
-  validates :name, uniqueness: true, presence: true
-  validates :name, :hand_baggage, :baggage, :cambios, :refunds, :child_discount, :miles, :seat, :cost, presence: true
-
+  validates :name, uniqueness: true
 end
