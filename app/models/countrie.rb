ea71e52ea85c027a2dd24125d245972a98393587
Fernando AntÃunez <fernandojesusantunez@gmail.com>
2021-10-26 12:41:54 -0400

    validaciones

--- app/models/countrie.rb
+++ app/models/countrie.rb
@@ -1,4 +1,5 @@
 class Countrie < ApplicationRecord
     has_many :states
     validates :name, uniqueness: true
+    validates :name, presence: true
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,5 +1,5 @@
 class State < ApplicationRecord
   belongs_to :countrie
   has_many :offers
-  validates :name, uniqueness: true
+  validates :name, presence: true
 end
