416ab5c4603f975015b123d8d155261c8cd2756c
Fernando Amundaray <fernandojal.une@gmail.com>
2021-10-26 10:23:03 -0400

    fixed width and height cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_224124) do
+ActiveRecord::Schema.define(version: 2021_10_25_232047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
+    t.bigint "rate_id", null: false
     t.bigint "user_id", null: false
+    t.bigint "state_id", null: false
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["rate_id"], name: "index_bookings_on_rate_id"
+    t.index ["state_id"], name: "index_bookings_on_state_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -59,6 +63,8 @@ ActiveRecord::Schema.define(version: 2021_10_25_224124) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "rates"
+  add_foreign_key "bookings", "states"
   add_foreign_key "bookings", "users"
   add_foreign_key "states", "countries", column: "countrie_id"
 end
