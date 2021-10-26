c83a87550edc55475b9f044afc2d4e6040c34876
genesis fernandez <gensdugarte@gmail.com>
2021-10-26 12:56:13 -0400

    ruta de rates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'pages/rate', to: 'rates#rate', as: :rates
+  get 'rates', to: 'rates#rate', as: :rates
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
