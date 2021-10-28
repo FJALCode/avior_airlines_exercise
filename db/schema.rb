3a10983b2bbced9bad58f7077365dac3c93cc5df
Alfredo Alejandro Villegas <xillegas@gmail.com>
2021-10-28 17:50:13 -0400

    relacionando estados con paises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_183506) do
+ActiveRecord::Schema.define(version: 2021_10_28_213004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_10_28_183506) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "iso2"
   end
 
   create_table "offers", force: :cascade do |t|
